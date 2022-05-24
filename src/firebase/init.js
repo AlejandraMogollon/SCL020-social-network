61b06753bcade9b85511dc400f58b54106056d97
AlejandraMogollon <alejandrame1505@gmail.com>
2022-05-24 19:23:44 -0400

    preparando archivo

---
+++ src/firebase/init.js
@@ -0,0 +1,8 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js';
+
+import config from './config.js';
+
+// Initialize Firebase
+initializeApp(config);
+
+export { initializeApp };
