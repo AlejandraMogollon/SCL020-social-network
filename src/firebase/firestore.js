dd050ba5a09f69fa724e8f63d04334cf721d18c3
AlejandraMogollon <alejandrame1505@gmail.com>
2022-06-10 09:52:14 -0400

    Like y edit funcionales

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -3,6 +3,7 @@ import {
   addDoc,
   Timestamp,
   getDocs,
+  getDoc,
   query,
   where,
   doc,
@@ -12,9 +13,11 @@ import {
   onSnapshot,
   orderBy,
   limit,
-} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
+  arrayRemove,
+  arrayUnion,
+} from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js';
 
-import { db } from "../firebase/init.js";
+import { db } from '../firebase/init.js';
 
 //parametros de lo q queremos guardar (ej "ada")
 export const createData = async (
@@ -26,18 +29,20 @@ export const createData = async (
   try {
     let postId = Math.random().toString(16).slice(2);
 
-    const docRef = await addDoc(collection(db, "post"), {
+    const docRef = await addDoc(collection(db, 'post'), {
       post: textPost2,
       date: Timestamp.now(),
       user: currentUser,
       mail: userEmail,
       nick: displayName,
       postId: postId,
+      LikeCount: 0,
+      UsersWhoLiked: [],
     });
     // ;
     return docRef.id;
   } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error adding document: ', e);
   }
 };
 
@@ -46,15 +51,15 @@ export const createData = async (
 //ahora a feed
 export const getUserData = async (userUid) => {
   const querySelector = await query(
-    collection(db, "user"),
-    where("id", "==", userUid)
+    collection(db, 'user'),
+    where('id', '==', userUid)
   );
   const userFirebase = await getDocs(querySelector);
   return userFirebase.docs[0].data();
 };
 
 export const getPost = async () => {
-  const querySnapshot = await getDocs(collection(db, "post"));
+  const querySnapshot = await getDocs(collection(db, 'post'));
   let postArr = [];
   querySnapshot.forEach((doc) => {
     const docuData = doc.data();
@@ -65,42 +70,61 @@ export const getPost = async () => {
   return postArr;
 };
 
+export const getPostById = async (postId) => {
+  const docRef = doc(db, 'post', postId);
+  const docPost = await getDoc(docRef);
+  if (docPost.exists()) {
+    // console.log('Document data:', docPost.data());
+    return {
+      postData: docPost.data(),
+      docRef,
+    };
+  } else {
+    // doc.data() will be undefined in this case
+    console.log('No such document!');
+  }
+};
+
 export const deletePost = async (id) => {
-  await deleteDoc(doc(db, "post", id));
-  console.log("entre");
+  await deleteDoc(doc(db, 'post', id));
+  console.log('entre');
 };
 
 //VALIDAR QUE TEXTO POST NO ESTE VACIO
 
 export const readPost = async (callback) => {
-  // onSnapshot(query(collection(db, "post"), orderBy("date", "desc")), callback);
-  onSnapshot(query(collection(db, "post"), orderBy("date", "desc")), (doc) => {
+  onSnapshot(query(collection(db, 'post'), orderBy('date', 'desc')), (doc) => {
     let array = [];
     doc.docs.forEach((post) => {
       array.push({ id: post.id, data: post.data() });
-      // console.log(post.id, post.data());
     });
     callback(array);
   });
 };
 
-// export const readPost2 = async (callback) => {
-//   onSnapshot(query(collection(db, "post"), orderBy("date", "desc")), callback);
-// };
-
-// export const readDELETE = async (callback, caca) => {
-//   query(collection(db, "post"), where("postId", "==", caca));
-// };
-
-// readDELETE((docu) => {
-//   console.log(docu);
-// });
-export const getUserData2 = async (callback) => {
-  const querySnapshot = await getDocs(collection(db, "post"));
-  // for (let i in querySnapshot) {
-  //   console.log(querySnapshot.docs[i]);
-  // }
+export const editPost = async (id, editPost) => {
+  await updateDoc(doc(db, 'post', id), { post: editPost });
+  console.log(editPost);
 };
-getUserData2((post) => {
-  console.log(post);
-});
+
+export const likeStatus = async (postId, userUid) => {
+  const postInfo = await getPostById(postId);
+  const arrUserLiked = postInfo.postData.UsersWhoLiked;
+  console.log('Arrr', arrUserLiked);
+  let likeCount = postInfo.postData.LikeCount;
+  if (arrUserLiked.includes(userUid)) {
+    // arrUserLiked: arrayRemove(userUid);
+    await updateDoc(postInfo.docRef, {
+      UsersWhoLiked: arrayRemove(userUid),
+      LikeCount: likeCount - 1,
+    });
+    return true;
+  } else {
+    // arrUserLiked: arrayUnion(userUid);
+    await updateDoc(postInfo.docRef, {
+      UsersWhoLiked: arrayUnion(userUid),
+      LikeCount: likeCount + 1,
+    });
+    return false;
+  }
+};
--- src/templates/feed.js
+++ src/templates/feed.js
@@ -1,13 +1,15 @@
-import { onNavigate } from "../router/router.js";
-import { auth } from "../firebase/init.js";
+import { onNavigate } from '../router/router.js';
+import { auth } from '../firebase/init.js';
 import {
   createData,
   deletePost,
   getPost,
   getUserData,
   readPost,
-} from "../firebase/firestore.js";
-import { logOut } from "../firebase/auth.js";
+  editPost,
+  likeStatus,
+} from '../firebase/firestore.js';
+import { logOut } from '../firebase/auth.js';
 
 const feed = async () => {
   const templateFeed = ` 
@@ -36,62 +38,109 @@ const feed = async () => {
     </main>
   </section> `;
   //TEMPLATE FEED A FEEDCONTAINER (DIV)
-  const feedContainer = document.createElement("div");
+  const feedContainer = document.createElement('div');
   feedContainer.innerHTML = templateFeed;
 
   //BOTON LOGOUT - ONCLICK => SYNC - SIGNOUT (FIREBASE) -> ONNAVIGATE(LOGIN)
-  const btnLogOut = feedContainer.querySelector(".btnLogOut");
-  btnLogOut.addEventListener("click", async () => {
+  const btnLogOut = feedContainer.querySelector('.btnLogOut');
+  btnLogOut.addEventListener('click', async () => {
     await logOut(auth);
-    onNavigate("/");
+    onNavigate('/');
   });
 
   //BOTON POST
-  const btnPost = feedContainer.querySelector(".post-btn");
-  let rootFeed = feedContainer.querySelector(".root-post");
-  // const textPost = feedContainer.querySelector('.text-post');
-  // const textPost2 = textPost.value;
+  const btnPost = feedContainer.querySelector('.post-btn');
+  let rootFeed = feedContainer.querySelector('.root-post');
 
   const renderTemplateFeed = (post) => {
-    // console.log(post);
-    rootFeed.innerHTML = "";
-    let postList = "";
+    rootFeed.innerHTML = '';
+    let postList = '';
     post.forEach(async (doc) => {
       let docData = doc.data;
       let docId = doc.id;
-      console.log(docData);
-      // console.log(doc.data());
+      // console.log(docId);
+      // console.log(docData);
       postList += `
       <div class="interaction-posted">
         <div class="posted-header">
           <img class="user-photo" src="https://www.eaclinic.co.uk/wp-content/uploads/2019/01/woman-face-eyes-500x500.jpg" alt="user-photo">
           <p class="user-name"> ${docData.nick}  </p>
           <img class="delete-icon"src="img/delete-icon.png" id=${docId} alt="delete-icon">
-          <img class="edit-icon"src="img/edit-icon.png" alt="edit-icon">
+          <i class="far fa-edit" id=${docId} ></i>
         </div>
         <p class="post-date" >${docData.date.toDate().toLocaleString()}</p> 
-        <textarea  class="posted-text" disabled=true> ${
-          docData.post
-        } </textarea >
+        <textarea  id=${docId}  class="posted-text" disabled=true> ${
+        docData.post
+      } </textarea >
             <div class="icons-posted">
-              <img class="heart-icon" src="img/like-icon.png" alt="heart-icon">
-              <p class="likes-count">0</p>
+              <img class="heart-icon" id=${docId} src="img/like-icon.png" alt="heart-icon">
+              <p class="likes-count">${docData.LikeCount}</p>
               <img class="comment-icon"src="img/comment-icon.png" alt="comment-icon">
+              <button class="cancel-edit-btn">Cancel</button>
+              <button id="${docId}" class="confirm-edit-btn">Save</button>
             </div>
           </div>`;
-      // console.log(doc.data());
     });
     rootFeed.innerHTML = postList;
-    const btnDelete = feedContainer.querySelectorAll(".delete-icon");
+    const btnsEdit = feedContainer.querySelectorAll('.fa-edit');
+
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        let textArea = feedContainer.querySelector('.posted-text');
+
+        console.log(textArea.value);
+        let btnConfirmEdit = feedContainer.querySelector('.confirm-edit-btn');
+        let btnCancelEdit = feedContainer.querySelector('.cancel-edit-btn');
+        textArea.disabled = false;
+        textArea.style.border = 'solid 2px white';
+        btnCancelEdit.style.visibility = 'visible';
+        btnConfirmEdit.style.visibility = 'visible';
+
+        btnCancelEdit.addEventListener('click', () => {
+          btnCancelEdit.style.visibility = 'hidden';
+          btnConfirmEdit.style.visibility = 'hidden';
+          textArea.disabled = true;
+          textArea.style.border = 'none';
+        });
+
+        btnConfirmEdit.addEventListener('click', async () => {
+          await editPost(btnConfirmEdit.id, textArea.value);
+          console.log(btnConfirmEdit.id.length);
+          console.log('confirm edit clicked');
+        });
+      });
+    });
+
+    //Button Like
+    const btnLike = feedContainer.querySelectorAll('.heart-icon');
+    let count = feedContainer.querySelector('.likes-count');
+    btnLike.forEach((buttonLike) => {
+      buttonLike.addEventListener('click', async (e) => {
+        const idPostLike = e.target.id;
+        const likeInteraction = await likeStatus(
+          idPostLike,
+          auth.currentUser.uid
+        );
+        if (!likeInteraction) {
+          buttonLike.src = 'img/liked-icon.png';
+          console.log('entre al if', buttonLike);
+        } else {
+          console.log('entre al else');
+          buttonLike.src = 'img/like-icon.png';
+        }
+      });
+    });
+
+    const btnDelete = feedContainer.querySelectorAll('.delete-icon');
     btnDelete.forEach((btn) => {
-      console.log("hola");
-      btn.addEventListener("click", async (e) => {
+      btn.addEventListener('click', async () => {
         console.log(btn.id);
         // let id = e.target.id;
-        // const deleteAlert = confirm("Are you sure you want delete this post?");
-        // if (deleteAlert === true) {
-        await deletePost(btn.id);
-        readPost(renderTemplateFeed);
+        const deleteAlert = confirm('Are you sure you want delete this post?');
+        if (deleteAlert === true) {
+          await deletePost(btn.id);
+          readPost(renderTemplateFeed);
+        }
         //   window.location.reload();
         // } else {
         //   alert("Your post was not eliminated!!");
@@ -101,51 +150,39 @@ const feed = async () => {
   };
 
   readPost(renderTemplateFeed);
-  btnPost.addEventListener("click", async () => {
-    // const rootFeed = feedContainer.querySelector('.root-post');
-    const textPost = feedContainer.querySelector(".text-post");
-    const textPost2 = textPost.value;
 
+  btnPost.addEventListener('click', async () => {
+    const textPost = feedContainer.querySelector('.text-post');
     const userData = await getUserData(auth.currentUser.uid);
     const postId = await createData(
       userData.id,
-      textPost2,
+      textPost.value,
       userData.mail,
       userData.nick
     );
-
     console.log(postId);
-    const subRoot = document.createElement("div");
-    subRoot.className = "interaction-posted";
+    const subRoot = document.createElement('div');
+    subRoot.className = 'interaction-posted';
     // subRoot.innerHTML = await templateCreatedLastPost(
     //   userData.nick,
     //   postId,
     //   textPost.value
     // );
-    const firstPost = rootFeed.querySelector(".interaction-posted");
-    // rootFeed.insertBefore(subRoot, firstPost);
 
-    // console.log("post button clicked", postId);
-    textPost.value = "";
+    textPost.value = '';
 
-    const btnEdit = feedContainer.querySelectorAll(".edit-icon");
-    btnEdit.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        console.log("edit-icon clicked");
-      });
-    });
-    const btnLike = feedContainer.querySelectorAll(".heart-icon");
+    const btnLike = feedContainer.querySelectorAll('.heart-icon');
     btnLike.forEach((btn) => {
-      btn.addEventListener("click", function () {
+      btn.addEventListener('click', function () {
         console.log(this);
-        this.src = "img/liked-icon.png";
+        this.src = 'img/liked-icon.png';
       });
     });
 
-    const btnComment = feedContainer.querySelectorAll(".comment-icon");
+    const btnComment = feedContainer.querySelectorAll('.comment-icon');
     btnComment.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        console.log("comment-icon clicked");
+      btn.addEventListener('click', () => {
+        console.log('comment-icon clicked');
       });
     });
   });
@@ -154,40 +191,3 @@ const feed = async () => {
 };
 
 export default feed;
-
-//al darle clic a post, nos traemos esa data y la usamos para llamarla dandole los .id, .mail .nick
-//y si queremos el nombre lo imprimimos en el html
-//no esta bien que sea cada vez que le demos clic al post, debemos buscar una forma de traerlo automaticamente al cargar el feed
-//cuando creemos los post es mejor usar un map que un forEach.
-
-//CREATE ARRAY DE POST
-
-// const arrayPost = await getPost(); FUNCIONANDO
-// const arrayPost = await getPost();
-// const readPost = arrayPost.sort((x, y) => {
-//   return y.date - x.date;
-// });
-
-// const postSorted = arrayPost.sort((x, y) => { //FUNCIONANDO
-//   return y.date - x.date;
-// });
-
-// if (postSorted.length > 0) {
-//   postSorted.forEach((post) => {
-//     rootFeed.innerHTML += `
-//     <div class="interaction-posted">
-//       <div class="posted-header">
-//         <img class="user-photo" src="https://www.eaclinic.co.uk/wp-content/uploads/2019/01/woman-face-eyes-500x500.jpg" alt="user-photo">
-//         <p class="user-name"> ${post.nick} </p>
-//         <img class="delete-icon"src="img/delete-icon.png" id=${post.id} alt="delete-icon">
-//         <img class="edit-icon"src="img/edit-icon.png" alt="edit-icon">
-//       </div>
-//       <p class="posted-text"> ${post.post} </p>
-//           <div class="icons-posted">
-//             <img class="heart-icon" src="img/like-icon.png" alt="heart-icon">
-//             <p class="likes-count">0</p>
-//             <img class="comment-icon"src="img/comment-icon.png" alt="comment-icon">
-//           </div>
-//         </div>`;
-//   });
-// }  FUNCIONANDO
